66e3f81313c8f3a14541c971d1fe9a3fb05f6cf6
fakebenjay <fakebenjay@gmail.com>
2017-03-30 10:41:28 -0400

    Refactored template

--- js/views/hotspotView.js
+++ js/views/hotspotView.js
@@ -5,14 +5,13 @@ class HotspotView {
   }
 
   static listItemTemplate(hotspot){
-    return '<li id="hotspot-details">' + '<div> Object ID: ' + hotspot.objectId + '</div>'
-    + '<div> SSID: ' + hotspot.ssid + '</div>'
-    + '<div> Location/Address: ' + hotspot.location + ', ' + hotspot.city + '</div>'
-    + '<div> Location Type: ' + hotspot.locationType + '</div>'
-    + '<div> Name: ' + hotspot.name + '</div>'
-    + '<div> Provider: ' + hotspot.provider + '</div>'
-    + hotspot.latitude + ', '
-    + hotspot.longitude
-    + '</li><br>'
+    return `<li id="hotspot-details"><div> Object ID: ${hotspot.objectId}</div>
+    <div> SSID: ${hotspot.ssid}</div>
+    <div> Location/Address: ${hotspot.location}, ${hotspot.city}</div>
+    <div> Location Type: ${hotspot.locationType}</div>
+    <div> Name: ${hotspot.name}</div>
+    <div> Provider: ${hotspot.provider}</div>
+    <div>${hotspot.latitude}, ${hotspot.longitude}</div>
+    </li><br>`
   }
 }
