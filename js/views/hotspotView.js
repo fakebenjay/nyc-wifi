777df4ed9d25725ebc84d1df70b3074ed30a45d9
fakebenjay <fakebenjay@gmail.com>
2017-03-31 13:17:25 -0400

    Hardcoded target in, displays list of hotspots, temp fix

--- js/views/hotspotView.js
+++ js/views/hotspotView.js
@@ -4,18 +4,18 @@ class HotspotView {
     let location = Here.whereAmI()
     console.log(location)
     console.log(listItems)
-    $target.html(listItems)
+    $("div#hotspots").html(listItems)
     // $target.html('<div id="mapHolder"></div>')
   }
 
   static listItemTemplate(hotspot) {
-    return `<li id="hotspot-details"><div> Object ID: ${hotspot.objectId}</div>
+    return `<div id="hotspot-details"><div> Object ID: ${hotspot.objectId}</div>
     <div> SSID: ${hotspot.ssid}</div>
     <div> Location/Address: ${hotspot.location}, ${hotspot.city}</div>
     <div> Location Type: ${hotspot.locationType}</div>
     <div> Name: ${hotspot.name}</div>
     <div> Provider: ${hotspot.provider}</div>
     <div>${hotspot.latitude}, ${hotspot.longitude}</div>
-    </li><br>`
+    </div><br>`
   }
 }
